e01f27dc0189d3a6ba4687739f5852c5977b0a55
nect2r <71915361+nect2r@users.noreply.github.com>
2023-01-02 11:19:25 +0900

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.javafx.book {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.javafx.book to javafx.fxml;
+    exports com.javafx.book;
+}
